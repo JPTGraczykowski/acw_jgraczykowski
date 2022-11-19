0154ac350e8636015993a3716a3dc0e0fc1c9386
JPTGraczykowski <216701@edu.p.lodz.pl>
2022-11-19 11:22:33 +0100

    Add query params and try to find time

--- lab2/source/server.py
+++ lab2/source/server.py
@@ -3,6 +3,7 @@ import http.server
 import socketserver
 import os
 from datetime import datetime
+from urlparse import urlparse
 
 #print('source code for "http.server":', http.server.__file__)
 
@@ -11,14 +12,19 @@ class web_server(http.server.SimpleHTTPRequestHandler):
     def do_GET(self):
 
         print(self.path)
+
+        query = urlparse(self.path).query
+        query_components = dict(qc.split("=") for qc in query.split("&"))
         
         if self.path == '/':
             self.protocol_version = 'HTTP/1.1'
             self.send_response(200)
             self.send_header("Content-type", "text/html; charset=UTF-8")
-            self.end_headers()            
-            self.wfile.write(b"Hello World!\n")
-            self.wfile.write(str.encode(datetime.now().strftime('%H:%M:%S')))
+            self.end_headers()
+            if len(query_components) == 0:
+                self.wfile.write(b"Hello World!\n")
+            if query_components['cmd'] == 'time':
+                self.wfile.write(str.encode(datetime.now().strftime('%H:%M:%S')))
         else:
             super().do_GET()
     
