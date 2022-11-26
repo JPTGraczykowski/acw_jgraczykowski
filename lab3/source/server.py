09c3f009c9bfe6dece2dce2d452a2f9bf9fd76e1
JPTGraczykowski <216701@edu.p.lodz.pl>
2022-11-26 10:36:38 +0100

    copy files from lab2

---
+++ lab3/source/server.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import http.server
+import socketserver
+import os
+from datetime import datetime
+from urllib.parse import urlparse, parse_qs
+
+#print('source code for "http.server":', http.server.__file__)
+
+class web_server(http.server.SimpleHTTPRequestHandler):
+    
+    def do_GET(self):
+
+        print(self.path)
+        path = urlparse(self.path)
+        query_components = parse_qs(urlparse(self.path).query)
+        
+        if path.path == '/':
+            self.protocol_version = 'HTTP/1.1'
+            self.send_response(200)
+            self.send_header("Content-type", "text/html; charset=UTF-8")
+            self.end_headers()
+            if not query_components:
+                self.wfile.write(b"Hello World!\n")
+            elif query_components.get('cmd', None) == ['time']:
+                self.wfile.write(str.encode(datetime.now().strftime('%H:%M:%S')))
+            elif query_components.get('cmd', None) == ['rev']:
+                string_to_reverse_param = query_components.get('str', None)
+                if string_to_reverse_param:
+                    string_to_reverse = string_to_reverse_param[0]
+                    reversed_string = string_to_reverse[::-1]
+                    self.wfile.write(str.encode(reversed_string))
+        else:
+            super().do_GET()
+    
+# --- main ---
+
+PORT = 4080
+
+print(f'Starting: http://localhost:{PORT}')
+
+tcp_server = socketserver.TCPServer(("",PORT), web_server)
+tcp_server.serve_forever()
